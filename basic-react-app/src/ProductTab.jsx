761de94171978a59ea4798cd345ecd2e95e22eed
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-01-26 19:16:39 +0530

    Initialize basic React app with Vite setup, including essential files and configurations. Add main components: App, Product, and ProductTab with styling. Include SVG assets for React and Vite logos, and set up ESLint for code quality.

---
+++ basic-react-app/src/ProductTab.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product.jsx";
+
+function ProductTab() {
+  let options = [<li>hi-tech</li>,<li>durable</li>,<li> fast</li>];
+  return (
+    <>
+    <Product title="phone" price={30000} features={options} />
+    {/* <Product title="laptop" price={40000}/>
+    <Product title="pen" price={1}/> */}
+    </>
+  );
+}
+
+export default ProductTab;
+
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,15 @@
+function Title() {
+  let name = "saquib";
+  return (
+  <div>
+    <p>2 * 2 = {2 * 2}</p>
+    <p>Hi , {name.toUpperCase()} </p>
+  </div>
+  );
+}
+
+
+
+//exports 
+//export default Title;
+export {Title};
