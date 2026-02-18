5ef25d78da197198a296e441dfe0501a001d781c
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-18 12:50:34 +0530

    Refactor Counter component to initialize count with a random value; enhance incCount function for better state updates and debugging.

--- react-state-class/src/Counter.jsx
+++ react-state-class/src/Counter.jsx
@@ -1,14 +1,27 @@
 import {useState} from "react";
+
+function init() {
+  console.log("init was executed")
+  return Math.random();
+}
+
 export default function Counter() {
  let [stateVariable, setStateVariable] = useState(10);
- let [count, setCount] = useState(0); //Initialization
- console.log("Component is rendered!");
- console.log(`count = ${count}`)
+ let [count, setCount] = useState(init); //Initialization
+ console.log("Component was rendered!");
+//  console.log(`count = ${count}`)
 
  let incCount = () => {
-  setCount(count+1);
-   console.log(`inside incCount ,count = ${count}`)
-
+  setCount((currCount) => {
+    return currCount + 1;
+  }); 
+  setCount((currCount) => {
+    return currCount + 1;
+  });
+  setCount((currCount) => {
+    return currCount + 1;
+  });
+  // setCount(25);
  };
  
 
