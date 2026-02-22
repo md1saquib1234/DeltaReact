6045a3ffab4740b6492a3bc71d4aedf02e2cc2a3
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-22 20:28:33 +0530

    feat: add Joker component to fetch and display random jokes

--- REACT-STATE-CLASS2/src/App.jsx
+++ REACT-STATE-CLASS2/src/App.jsx
@@ -6,11 +6,12 @@ import Form from './Form'
 import CommentsForm from './CommentsForm'
 import Comments from './Comments'
 import Counter from './Counter'
+import Joker from './Joker'
 
 function App() {
   return (
   <>
-  <Counter></Counter>
+  <Joker></Joker>
  
        
     </>
