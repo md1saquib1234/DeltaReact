60b3283e8b918e76759aa20b4e4eaa30c2a98e1d
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-22 19:43:59 +0530

    feat: add Counter component and integrate it into App for interactive counting

--- REACT-STATE-CLASS2/src/App.jsx
+++ REACT-STATE-CLASS2/src/App.jsx
@@ -5,11 +5,12 @@ import './App.css'
 import Form from './Form'
 import CommentsForm from './CommentsForm'
 import Comments from './Comments'
+import Counter from './Counter'
 
 function App() {
   return (
   <>
-  <Comments></Comments>
+  <Counter></Counter>
  
        
     </>
---
+++ REACT-STATE-CLASS2/src/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+
+export default function Counter() {
+
+  let [count , setCount] = useState(0);
+
+  let incCount = () => {
+    setCount((currCount) => currCount + 1);
+  };
+
+  useEffect((function printSomething() {
+    console.log("This is a side-effect");
+  }));
+
+  return (
+    <div>
+      <h3>Count = {count}</h3>
+      <button onClick={incCount}>+1</button>
+    </div>
+  );
+}
