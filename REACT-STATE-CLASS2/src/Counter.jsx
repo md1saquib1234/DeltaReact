494eee4c984a2db51588467201e551acfa83c62d
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-22 19:53:53 +0530

    feat: refactor Counter component to manage separate counts for countx and county with individual increment functions

--- REACT-STATE-CLASS2/src/Counter.jsx
+++ REACT-STATE-CLASS2/src/Counter.jsx
@@ -2,20 +2,28 @@ import { useState, useEffect } from "react"
 
 export default function Counter() {
 
-  let [count , setCount] = useState(0);
+  let [countx , setCountx] = useState(0);
+  let [county , setCounty] = useState(0);
 
-  let incCount = () => {
-    setCount((currCount) => currCount + 1);
+  let incCountx = () => {
+    setCountx((currCount) => currCount + 1);
   };
 
-  useEffect((function printSomething() {
+   let incCounty = () => {
+    setCounty((currCount) => currCount + 1);
+  };
+
+  useEffect(
+    function printSomething() {
     console.log("This is a side-effect");
-  }));
+  },[] );
 
   return (
     <div>
-      <h3>Count = {count}</h3>
-      <button onClick={incCount}>+1</button>
+      <h3>Countx = {countx}</h3>
+      <button onClick={incCountx}>+1</button>
+      <h3> County = {county}</h3>
+      <button onClick={incCounty}>+1</button>
     </div>
   );
 }
