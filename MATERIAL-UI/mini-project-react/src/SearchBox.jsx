8fbd37cfdb13c44bcb35dc03bb397a5e3bacc8b2
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-23 20:07:40 +0530

    feat: enhance SearchBox to fetch and display weather information based on city input

--- MATERIAL-UI/mini-project-react/src/SearchBox.jsx
+++ MATERIAL-UI/mini-project-react/src/SearchBox.jsx
@@ -5,6 +5,25 @@ import {useState} from "react";
 
 export default function SearchBox() {
   let [city, setCity] = useState("");
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "8c5c32fbff76a09b9a15b77106eacd0c";
+
+  let getWeatherInfo = async () => {
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+    let result = {
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelsLike: jsonResponse.main.feel_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  }
+
+  
 
   let handleChange = (event) => {
     setCity(event.target.value);
@@ -14,7 +33,8 @@ export default function SearchBox() {
     evt.preventDefault();
     console.log(city);
     setCity("");
-  }
+    getWeatherInfo();
+  };
 
   return (
     <div>
