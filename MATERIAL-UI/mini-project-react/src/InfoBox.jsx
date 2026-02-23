fe72e1951d727f1bc0ef0257a908d48f9eaa1708
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-24 01:20:17 +0530

    fix: correct formatting in weather description paragraph in InfoBox component

--- MATERIAL-UI/mini-project-react/src/InfoBox.jsx
+++ MATERIAL-UI/mini-project-react/src/InfoBox.jsx
@@ -36,12 +36,13 @@ export default function InfoBox() {
          <p>Humidity = {info.humidity}</p>
          <p>Min Temp = {info.tempMin}&deg;C</p>
          <p>Max Temp = {info.tempMax}&deg;C</p>
-         <p> The weather can be described as  
-           <i>{info.weather}</i> and feels like {info.feelsLike}&deg;C</p>
+         <p>The weather can be described as  
+           <i>{ info.weather}</i> and feels like {info.feelsLike}&deg;C</p>
         </Typography>
       </CardContent>
     </Card>
     </div>
     </div>
+
   );
 }  
