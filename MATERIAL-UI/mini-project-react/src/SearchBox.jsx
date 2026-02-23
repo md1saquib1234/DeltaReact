f0d588b5065b8504088c46ce2712f5bfbeaa7d66
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-02-24 01:12:56 +0530

    feat: enhance InfoBox component with weather details and styling

--- MATERIAL-UI/mini-project-react/src/SearchBox.jsx
+++ MATERIAL-UI/mini-project-react/src/SearchBox.jsx
@@ -17,7 +17,7 @@ export default function SearchBox() {
       tempMin: jsonResponse.main.temp_min,
       tempMax: jsonResponse.main.temp_max,
       humidity: jsonResponse.main.humidity,
-      feelsLike: jsonResponse.main.feel_like,
+      feelsLike: jsonResponse.main.feels_like,
       weather: jsonResponse.weather[0].description,
     };
     console.log(result);
